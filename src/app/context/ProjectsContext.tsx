3eef4bed399ec6832c03c96b43a6cea56764a947
Luma Gabriela <lumagabriela3331@gmail.com>
2025-10-01 13:00:39 -0300

    feat: adiciona todos os elementos no projectcontext

--- src/app/context/ProjectsContext.tsx
+++ src/app/context/ProjectsContext.tsx
@@ -3,6 +3,31 @@
 
 import { createContext, useContext } from "react"
 
+interface StudentProps {
+  name: string
+  institution: string
+  campus: string
+  email: string
+  curriculumLink: string
+  imageUrl: string
+}
+interface ExStudentProps {
+  name: string
+  institution: string
+  campus: string
+  email: string
+  curriculumLink: string
+  imageUrl: string
+}
+interface TeacherProps {
+  name: string
+  institution: string
+  campus: string
+  email: string
+  curriculumLink: string
+  imageUrl: string
+}
+
 interface TCCProps {
   title: string
   link: string
@@ -12,9 +37,6 @@ interface TCCProps {
   year: string
 }
 
-// 2. Defina os dados que serão compartilhados.
-
-// 1. Defina a interface (type) para seus projetos aqui, para manter tudo organizado.
 export interface Project {
   title: string
   description: string
@@ -25,6 +47,470 @@ export interface Project {
 }
 
 // 2. Defina os dados que serão compartilhados.
+const students: StudentProps[] = [
+  {
+    name: "Adriane da Costa Ferreira",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/0201229307900067",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1564966Z5",
+  },
+  {
+    name: "Augusto Fadanelli de Sousa",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/4905967797980110",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8077169T2",
+  },
+  {
+    name: "Aymmée Nadine Diniz Neris",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/2629359369799722",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2703499D5",
+  },
+  {
+    name: "Carlos Alberto Barbosa de Almeida Junior",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/4624599784919056",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1546828T8",
+  },
+  {
+    name: "Carlos Henrique Barreto Mareco",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/3855670927634348",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2863059J8",
+  },
+  {
+    name: "Celeste dos Santos Dantas",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/3115743863912814",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1176961E2",
+  },
+  {
+    name: "Dario Rodrigues do Carmo",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/1169535933506040",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1582105J0",
+  },
+
+  {
+    name: "Eduardo Sousa Lima",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/3954320570829034",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8254705U6",
+  },
+  {
+    name: "Eduardo Teixeira Flexa",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/0262199390232721",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1539994D7",
+  },
+  {
+    name: "Enzo Cardoso de Castilho",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/0919329108329523",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2547927T7",
+  },
+  {
+    name: "Fernando Rafael Serra Neves",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/8768694050430103",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8145617U2",
+  },
+  {
+    name: "Gabriel Enrique Almeida Gonçalves",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/6118298421134507",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K9052777Y7",
+  },
+  {
+    name: "Gabriela Felipa da Silva Souza",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/6676365359182975",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2406638Y4",
+  },
+  {
+    name: "Guilherme Cosmo Lins de Souza",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/7683827799391036",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K9360787E2",
+  },
+  {
+    name: "Heloysa Gama Uchoa",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/0368761867719344",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1567493H0",
+  },
+  {
+    name: "Higor Souza da Silva",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/4972084328427721",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1124089J2",
+  },
+  {
+    name: "Jhonathan Willan Costa Lobato",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/2338212383461581",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=0425737136",
+  },
+  {
+    name: "João Pedro Viana Bezerra",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/8605422148199555",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2896833J8",
+  },
+  {
+    name: "João Victor Machado de Andrade",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/8539615512841442",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2726689P6",
+  },
+  {
+    name: "Johnathan Rocha de Sousa",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/3678750924075733",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4404089Z4",
+  },
+  {
+    name: "José Clêmer Teixeira Pantoja",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/1617755992099891",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2896102D7",
+  },
+  {
+    name: "José Kaynã Pereira de Sousa Bitencourt",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/8979398424386518",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1563227E0",
+  },
+  {
+    name: "José Olimpio de Freitas Dias Júnior",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/6283613146447214",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8080585T4",
+  },
+  {
+    name: "Josilene Aline Soares Ferreira de Oliveira",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/6490034202076915",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1511232Z6",
+  },
+  {
+    name: "Julia Vitoria Costa dos Santos",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/9085951782034759",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1545016Z6",
+  },
+  {
+    name: "Katlen Vanessa dos Santos da Silva",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/8478985982111322",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1160296T9",
+  },
+  {
+    name: "Kauê de Magalhães Brandão",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/5709405973358001",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1536946Y9",
+  },
+  {
+    name: "Lorena Roberta Nunes Guimarães",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/8067840813058871",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2406465Y1",
+  },
+  {
+    name: "Lucas Havel Nobre Maia",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/0003377526836527",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1508336H3",
+  },
+  {
+    name: "Lucas Soutelo Souto Pinheiro",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/4400152259018443",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2896840T9",
+  },
+  {
+    name: "Luis Henrique Campos Mafra",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/9961825338528812",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1545234A4",
+  },
+  {
+    name: "Luiz Felipe Moreira dos Anjos",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/6967787408321484",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1561560P9",
+  },
+  {
+    name: "Luma Gabriela Andrade da Silva",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/4278102715514258",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1546897P6",
+  },
+  {
+    name: "Marco Antonio da Silva Neves Filho",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/7123969561902651",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4373842E3",
+  },
+  {
+    name: "Marcus Vinícyus Santos Figueira",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/1581056759223655",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1546822D2",
+  },
+  {
+    name: "Maria Eduarda Smith Santana",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/6344082541870444",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1561962J3",
+  },
+  {
+    name: "Mariane da Silva Azevedo",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/6849698686793236",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2445217D7",
+  },
+  {
+    name: "Patrick Miranda dos Santos",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/0003988321971896",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1530663D5",
+  },
+  {
+    name: "Paula Maria Lacerda de Oliveira Costa",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/3684202517504539",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1544794A0",
+  },
+  {
+    name: "Victor Gabriel Gomes Bahia",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/1336036540136342",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1109471Z0",
+  },
+  {
+    name: "Yasmim Pereira Melo",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/9750144066062051",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K1544291J9",
+  },
+]
+const exStudents: ExStudentProps[] = [
+  {
+    name: "Aíla Taynara Santos Moreira Maciel",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/0285174097186394",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2100157A0",
+  },
+  {
+    name: "Carlos Eduardo Brito da Silva dos Santos",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/7238237839067724",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8804965Z9",
+  },
+  {
+    name: "Edson Monteiro Neto",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/1270283553777603",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8151323E4",
+  },
+  {
+    name: "Eduardo Luigi Tavares da Silva Ciuffi",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: " http://lattes.cnpq.br/8996710321421005",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2701533T0",
+  },
+  {
+    name: "Eduardo Rodrigues Nogueira",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "email@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/3772381642880757",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8068695T1",
+  },
+  {
+    name: "Erick da Cunha Sampaio",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/3394751225824953",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8169200T6",
+  },
+  {
+    name: "Gabriel Melo da Silveira",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/1810258668918861",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8151934Y3",
+  },
+  {
+    name: "Edson Monteiro Neto",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/1270283553777603",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8151323E4",
+  },
+  {
+    name: "Juliana Leal Alves",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/4617405056839703",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8913815D6",
+  },
+  {
+    name: "Luís César Martins Amaral",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/8829459581349801",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K2109801U5",
+  },
+  {
+    name: "Mateus Bezerra da Silva",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/0650268746194141",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8846994Y0",
+  },
+  {
+    name: "Matheus Costa Silva",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/9735600071757777",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8972040P1",
+  },
+]
+const teachers: TeacherProps[] = [
+  {
+    name: "Julio Cezar Costa Furtado",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/9175382702970383",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4258925P6",
+  },
+  {
+    name: "Rafael Pontes Lima",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/3248608898278519",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4133837P0",
+  },
+  {
+    name: "Adolfo Francesco de Oliveira Colares",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/7694692886453388",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4202733D9",
+  },
+  {
+    name: "Anderson dos Santos Guerra",
+    institution: "Unifap",
+    campus: "Campus Unifap",
+    email: "julio@unifap.br",
+    curriculumLink: "http://lattes.cnpq.br/7801947090551930",
+    imageUrl: "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K8583463P7",
+  },
+]
+
 const tccs: TCCProps[] = [
   {
     title:
@@ -155,6 +641,7 @@ const tccs: TCCProps[] = [
     year: "2019",
   },
 ]
+
 const projectsData: Project[] = [
   {
     title: "Projeto SERDE - Software Engineering: Research, Development and Education",
@@ -182,11 +669,18 @@ const projectsData: Project[] = [
   },
 ]
 
-// 3. Crie o Contexto
-// O valor inicial aqui não é tão importante, pois o Provider irá sobrescrevê-lo.
-const ProjectsContext = createContext<{ projects: Project[]; tccs: TCCProps[] }>({
+const ProjectsContext = createContext<{
+  projects: Project[]
+  tccs: TCCProps[]
+  teachers: TeacherProps[]
+  exStudents: ExStudentProps[]
+  students: StudentProps[]
+}>({
   projects: [],
   tccs: [],
+  teachers: [],
+  exStudents: [],
+  students: [],
 })
 
 // 4. Crie o Componente Provider
@@ -194,7 +688,13 @@ const ProjectsContext = createContext<{ projects: Project[]; tccs: TCCProps[] }>
 export function ProjectsProvider({ children }: { children: React.ReactNode }) {
   // Neste caso, os dados são estáticos, então não precisamos de um `useState`.
   // Se os projetos pudessem ser alterados (adicionados, removidos), usaríamos useState aqui.
-  const value = { projects: projectsData, tccs: tccs }
+  const value = {
+    projects: projectsData,
+    tccs: tccs,
+    teachers: teachers,
+    exStudents: exStudents,
+    students: students,
+  }
 
   return <ProjectsContext.Provider value={value}>{children}</ProjectsContext.Provider>
 }
