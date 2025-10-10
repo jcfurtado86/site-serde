534d6af5a3a09b58ba7a6d5fe81ffa4d28159873
Luma Gabriela <lumagabriela3331@gmail.com>
2025-10-10 18:08:43 -0300

    Update page.tsx

--- src/app/(pages)/parcerias/page.tsx
+++ src/app/(pages)/parcerias/page.tsx
@@ -7,7 +7,7 @@ export default function ProjetosPesquisa() {
   return (
     <main className="pt-20 bg-gray-50">
       <Breadcrumb items={[{ label: "Parcerias", href: "/parcerias" }]} />
-      <section className="bg-gradient-to-b from-gray-50 to-white py-3 sm:py-10">
+      <section className="bg-gradient-to-b from-gray-50 to-white py-3 sm:py-20">
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <h2 className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-black text-center mb-3 sm:mb-10">
             Parcerias
